3a7b5350700eb64b7eab9cc0c2593a427875f60e
Miguel Rivera <156738902+MiguelRivera016@users.noreply.github.com>
2024-03-27 13:43:09 -0600

    Version Final invitacion
    V1

---
+++ script.js
@@ -0,0 +1,87 @@
+const buttonNo = document.querySelector('#no')
+const buttonYes = document.querySelector('#yes')
+
+let fontSize = 2
+
+let messages = [
+  'Esta segura?',
+  'Pienselo bien',
+  'Pienselo muy bien, pueda que sea bonito',
+  'Que tal, y las risas no falten',
+  'El boton de Si, se mira mas misterioso'
+]
+
+buttonNo.addEventListener('click', () => {
+  fontSize = fontSize + .5
+  buttonYes.style.fontSize = `${fontSize}rem`
+
+  const indexRandom = Math.floor(Math.random() * messages.length)
+
+  buttonNo.textContent = messages[indexRandom]
+})
+
+buttonYes.addEventListener('click', () => {
+  document.querySelector('#message').style.display = 'flex'
+})
+
+// read custom message from query strings
+// Tutorial -> https://youtu.be/6ojp1iWUKw8
+
+const urlSearchParams = new URLSearchParams(window.location.search)
+
+const messageCustom = urlSearchParams.get('message')
+
+if (messageCustom) {
+
+  const mainMessageElement = document.querySelector('#mainMessage')
+  mainMessageElement.textContent = decodeURI(messageCustom)
+}
+
+// the tutorial starts here
+
+const btnOpenElement = document.querySelector('#open')
+const btnCloseElement = document.querySelector('#close')
+
+btnCloseElement.disabled = true
+
+
+
+btnOpenElement.addEventListener('click', ()=> {
+  btnOpenElement.disabled = true
+  btnCloseElement.disabled = false
+  const coverElement = document.querySelector('.cover')
+  coverElement.classList.add('open-cover')
+
+  setTimeout(()=>{
+    //
+    coverElement.style.zIndex = -1
+    
+    const paperElement = document.querySelector('.paper')
+    paperElement.classList.remove('close-paper')
+    paperElement.classList.add('open-paper')
+
+    // animacion del corazón
+    const heartElement = document.querySelector('.heart')
+    heartElement.style.display = 'block'
+  
+  }, 500)
+
+})
+btnCloseElement.addEventListener('click', ()=> {
+  btnOpenElement.disabled = false
+  btnCloseElement.disabled = true
+
+  const coverElement = document.querySelector('.cover')
+  const paperElement = document.querySelector('.paper')
+  paperElement.classList.remove('open-paper')
+  paperElement.classList.add('close-paper')
+  
+  setTimeout(()=>{
+    coverElement.style.zIndex = 0
+    coverElement.classList.remove('open-cover')
+
+    // animacion del corazón
+    const heartElement = document.querySelector('.heart')
+    heartElement.style.display = 'none'
+  },500)
+})
